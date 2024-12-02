a2e520cdd327f91eae55d82a4071f6974d08180a
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-02 12:05:42 +0200

    frontend <> getting projects through request

--- react-app/src/context/projectsContext.jsx
+++ react-app/src/context/projectsContext.jsx
@@ -1,10 +1,26 @@
+import axios from "axios"
 import { createContext, useEffect, useState } from "react";
 
 export const projectsContext = createContext();
 
 const projectsProvider = ({children})=>{
+    const [projects, setProjects] =useState([]);
+
+    useEffect(()=>{
+        axios.get("http://127.0.0.1:8000/api/projects")
+        .then((response)=>{
+
+            setProjects(response.data)
+        }
+        )
+    },[])
+
     return(
-        <projectsContext.Provider>
+        <projectsContext.Provider
+        values={{
+            projects: projects
+        }}
+        >
         {children}
       </projectsContext.Provider>
     )
