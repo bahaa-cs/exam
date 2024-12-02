665b2d7eb5d43abfe6adcdb46bd9795f07448500
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-02 11:28:23 +0200

    backend <> fixing bug in controller + creating rout to get users

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Users;
+use App\Models\User;
 
 class UserController extends Controller
 {
     function get_users() {
-        $users = Users::all();
+        $users = User::all();
 
         return response()->json([
             "users" => $users
