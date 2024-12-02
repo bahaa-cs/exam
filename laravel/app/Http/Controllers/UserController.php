6f002417ee5ea7cb8380e05aef746c2f1e66ad69
bahaaCs <bahaaaboukhalil@gmail.com>
2024-12-02 11:22:30 +0200

    backend <> creating controller function to get users

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Users;
 
 class UserController extends Controller
 {
-    //
+    function get_users() {
+        $users = Users::all();
+
+        return response()->json([
+            "users" => $users
+        ]);
+    }
 }
